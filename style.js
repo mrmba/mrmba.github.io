0524570b7c8cfe35457281a73e16feb435d8297c
Mary Rachael Brown <maryrachaelbrown@gmail.com>
2018-03-13 15:14:56 -0400

    added background, SM buttons, header

---
+++ style.js
@@ -0,0 +1,11 @@
+(function() {
+    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame ||
+    function(callback) {
+        window.setTimeout(callback, 1000 / 60);
+    };
+    window.requestAnimationFrame = requestAnimationFrame;
+})();
+
+
+var flakes = [],
+    canvas = document.getElementBy
